116a095c1dc86721fd95c402073ac23e662d6937
stas Shalamov <shalamov.stas1992@gmail.com>
2019-11-29 00:35:56 +0200

    Add project files.

---
+++ Console1/Program.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var items = new List<Item>(1000000);
+            for (int i = 0; i < 1000000; i++)
+            {
+                var item = new Item()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = $"UserName_{i}" + new string('a', 100),
+                    Adress = $"UserAdress_{i}" + new string('a', 100),
+                    Info = $"Info_{i}" + new string('a', 100),
+                    Images = "Images_" + new string('a', 100)
+                };
+                items.Add(item);
+            }
+
+            string text = JsonConvert.SerializeObject(items);
+
+            File.WriteAllText("RentFlatDB.txt", text);
+
+        }
+    }
+
+
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public string Info { get; set; }
+        public string Images { get; set; }
+
+    }
+}
---
+++ RentApartment/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using Newtonsoft.Json;
+using RentApartment.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RentApartment
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string Message { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            StartControl startControl = new StartControl();
+            Container.Children.Add(startControl);
+            startControl.Label.Text = "Loading";
+
+            new Thread(() => LoadDB(startControl)).Start();
+
+            
+        }
+
+        private void LoadDB(StartControl startControl)
+        {
+            string tempFolder = System.IO.Path.GetDirectoryName(Environment.GetFolderPath(Environment.SpecialFolder.Desktop)) + @"\Documents\RentFlatData\";
+            string dbFileName = "RentFlatDB.txt";
+            string fullDBFilePath = tempFolder + "\\" + dbFileName;
+            if (!Directory.Exists(tempFolder))
+            {
+                Directory.CreateDirectory(tempFolder);
+                return;
+            }
+            if (!File.Exists(fullDBFilePath))
+            {
+                return;
+            }
+
+            using (StreamReader reader = new StreamReader(fullDBFilePath))
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    startControl.Label.Text = "File in process";
+                });
+                string data = reader.ReadToEnd();
+                Dispatcher.Invoke(() =>
+                {
+                    startControl.Label.Text = "Data parcing";
+                });
+                if (data != null)
+                {
+                    List<Item> items = JsonConvert.DeserializeObject<List<Item>>(data);
+                    Message = items.Last<Item>().Name;
+                }
+
+                Dispatcher.Invoke(() =>
+                {
+                    startControl.Label.Text = "Loaded";
+                });
+
+            }
+        }
+    }
+}
